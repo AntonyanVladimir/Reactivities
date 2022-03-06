ed3d78855d98788113277c8bff997a75962e5f83
vladimirantonyan <antonyanvladimir1992@gmail.com>
2022-03-06 14:39:29 +0100

    walkling skeleton 2

--- api/Program.cs
+++ api/Program.cs
@@ -30,7 +30,6 @@ namespace API
             }
             await host.RunAsync();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
