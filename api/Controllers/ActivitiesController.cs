6d275bc313c6c7732ff8a2128b838e5ac592257f
vantonyan <vantonyan@werkstofftechnik.com>
2022-03-24 17:08:41 +0100

    edit and delete

--- api/Controllers/ActivitiesController.cs
+++ api/Controllers/ActivitiesController.cs
@@ -31,6 +31,12 @@ namespace api.Controllers
 
             return Ok(await Mediator.Send(new Create.Command{ Activity = activity})); 
         }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity){
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{ Activity = activity}));
+        }
+
 
     }
 }
